172f113ad55939347726c14e826581fe14048cbf
Ondřej Niesner <ondra.niesner@seznam.cz>
2019-10-30 12:57:57 +0100

    Update config.php

--- config.php
+++ config.php
@@ -19,6 +19,4 @@ $cf["anti-vpn"] = [
 	"ignored_groups" => array(51594, 51618, 52335, 51601, 51606, 51620, 51602, 51607, 51604, 51608, 51605, 51609, 51592, 51622, 51610),
 	"kick_message" => "VPN Is not allowed!"
 ];
-
-return $cf;
 ?>
