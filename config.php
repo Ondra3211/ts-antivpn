13ac71186461cfe5fac7a3f51cb6a10754c16ba6
Ondřej Niesner <ondra.niesner@seznam.cz>
2019-10-30 12:58:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -11,9 +11,8 @@ $cf["connect"] = [
 	"default_channel" => false /* false to disable */
 ];
 
-/**
- * https://iphub.info/
- */
+// https://iphub.info
+
 $cf["anti-vpn"] = [
 	"API-Key" => "",
 	"ignored_groups" => array(51594, 51618, 52335, 51601, 51606, 51620, 51602, 51607, 51604, 51608, 51605, 51609, 51592, 51622, 51610),
